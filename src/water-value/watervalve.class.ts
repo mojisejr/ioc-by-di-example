fb26de7e0fdbbaf6aa077feb20ad8436140e2fd5
nonthask laoluerat <nonthasak@Macintosh.local>
2021-02-01 00:11:01 +0700

    second commit

--- src/water-value/watervalve.class.ts
+++ src/water-value/watervalve.class.ts
@@ -9,3 +9,12 @@ export class WaterValve {
     console.log("ปิดวาล์วด้วยมือ");
   }
 }
+
+export class ServoWaterValve {
+  open() {
+    console.log("เปิดวาล์วด้วย Servo Motor");
+  }
+  close() {
+    console.log("ปิดวาล์วด้วย Servo Motor");
+  }
+}
